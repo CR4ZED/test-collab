4ed35e0515f6694b447e4ff7e12395f144584b86
CR4ZED <cleanmonk13@gmail.com>
2021-08-12 09:50:13 +0530

    adding 3rd log statement

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
-console.log('edited by captain mojo')
-console.log('edited by ankush')
+console.log("edited by captain mojo");
+console.log("edited by ankush");
+
+console.log("the third log statement made by ankush via cmd");
